73d34dfba63599eaae81e8dd5f7a9d75eb0b636c
mesantamaria <mesantamaria@uc.cl>
2025-02-23 20:43:15 -0300

    [Solar Panels] Add calculator

---
+++ panelsFittingCalculator.py
@@ -0,0 +1,44 @@
+def getNumberOfPanelsThatFitOnRoof(xRoof, yRoof, xPanel, yPanel):
+    return max(
+        _getNumberOfPanelsThatFitOnRoof(xRoof, yRoof, xPanel, yPanel),
+        _getNumberOfPanelsThatFitOnRoof(xRoof, yRoof, xPanel, yPanel))
+
+def _getNumberOfPanelsThatFitOnRoof(xRoof, yRoof, xPanel, yPanel):
+    """
+    This function returns the maximum number of panels that fit on
+    a roof of the specified dimensions.
+    It works by first calculating the amount of panels that fit in
+    one row (horizontally) and in one column (vertically). We then
+    multiply both of these numbers to get the number of panels that
+    we can place next to each other on the roof while maintaining
+    the same panel rotation. Lastly, we calculate how many
+    90°-rotated-panels we can place on the remaining space and
+    we add it to the count.
+    """
+    totalPanelsThatFit = _getPanelsThatFitWithoutRotating(
+        xRoof, yRoof, xPanel, yPanel)
+    # Check if 90 degrees rotated panel fits in remaining space of row
+    remainingHorizontalSpace = xRoof % xPanel
+    if (remainingHorizontalSpace) >= yPanel:
+        panelsThatFitInRemainingColumn = _getPanelsThatFitWithoutRotating(
+            remainingHorizontalSpace, yRoof, yPanel, xPanel)
+        totalPanelsThatFit += panelsThatFitInRemainingColumn
+    # Check if 90 degrees rotated panel fits in remaining space of column
+    remainingVerticalSpace = yRoof % yPanel
+    if (remainingVerticalSpace) >= xPanel:
+        panelsThatFitInRemainingRow = _getPanelsThatFitWithoutRotating(
+            xRoof, remainingVerticalSpace, yPanel, xPanel)
+        totalPanelsThatFit += panelsThatFitInRemainingRow
+
+    return totalPanelsThatFit
+
+def _getPanelsThatFitWithoutRotating(xRoof, yRoof, xPanel, yPanel):
+    panelsPerRow = xRoof // xPanel
+    panelsPerColumn = yRoof // yPanel
+    return panelsPerRow * panelsPerColumn
+
+print(getNumberOfPanelsThatFitOnRoof(2, 4, 1, 2))
+print(getNumberOfPanelsThatFitOnRoof(3, 5, 1, 2))
+print(getNumberOfPanelsThatFitOnRoof(2, 2, 1, 10))
+print(getNumberOfPanelsThatFitOnRoof(5, 11, 1, 3))
+print(getNumberOfPanelsThatFitOnRoof(1600, 1230, 137, 95))
